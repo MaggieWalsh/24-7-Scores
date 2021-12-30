7462e49e49b325433277da542c010a6eec3cca5a
Maggie Walsh <miggieholland@gmail.com>
2021-12-30 13:23:53 +0000

    Remove badge from league table

--- assets/js/league-table.js
+++ assets/js/league-table.js
@@ -45,38 +45,38 @@ function addRow(item) {
     element2.textContent = item.team.name;
     cell2.appendChild(element2);
 
+    // let cell3 = row.insertCell(2);
+    // let element3 = document.createElement("img");
+    // element3.src = item.team.crestUrl;
+    // cell3.appendChild(element3);
+
     let cell3 = row.insertCell(2);
-    let element3 = document.createElement("img");
-    element3.src = item.team.crestUrl;
+    let element3 = document.createElement("p");
+    element3.textContent = item.playedGames;
     cell3.appendChild(element3);
 
     let cell4 = row.insertCell(3);
     let element4 = document.createElement("p");
-    element4.textContent = item.playedGames;
+    element4.textContent = item.won;
     cell4.appendChild(element4);
 
     let cell5 = row.insertCell(4);
     let element5 = document.createElement("p");
-    element5.textContent = item.won;
+    element5.textContent = item.lost;
     cell5.appendChild(element5);
 
     let cell6 = row.insertCell(5);
     let element6 = document.createElement("p");
-    element6.textContent = item.lost;
+    element6.textContent = item.draw;
     cell6.appendChild(element6);
 
     let cell7 = row.insertCell(6);
     let element7 = document.createElement("p");
-    element7.textContent = item.draw;
+    element7.textContent = item.goalDifference;
     cell7.appendChild(element7);
 
     let cell8 = row.insertCell(7);
     let element8 = document.createElement("p");
-    element8.textContent = item.goalDifference;
+    element8.textContent = item.points;
     cell8.appendChild(element8);
-
-    let cell9 = row.insertCell(8);
-    let element9 = document.createElement("p");
-    element9.textContent = item.points;
-    cell9.appendChild(element9);
 }
