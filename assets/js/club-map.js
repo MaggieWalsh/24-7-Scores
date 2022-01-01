dbb165d82c7eb4946906b24098df53a814393677
Maggie Walsh <miggieholland@gmail.com>
2022-01-01 14:47:51 +0000

    Get map working, and set center

--- assets/js/club-map.js
+++ assets/js/club-map.js
@@ -1,1 +1,9 @@
 "use strict"
+let map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 53.430, lng: -2.961 },
+        zoom: 12,
+    });
+}
