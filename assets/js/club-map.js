689a7a06ba3244c34a3dc85ea77cb02c47d76204
Maggie Walsh <miggieholland@gmail.com>
2022-01-06 16:16:29 +0000

    Add markers to premier league stadium locations

--- assets/js/club-map.js
+++ assets/js/club-map.js
@@ -1,11 +1,11 @@
 "use strict"
-let map;
+// let map;
 
 function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 53.430, lng: -2.961 },
+    // Map options
+    const options = {
         zoom: 12,
-        // Add night mode styling from google maps docs, maybe later change to match site colours
+        center: { lat: 53.430, lng: -2.961 },
         styles: [
             { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
             { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
@@ -86,10 +86,60 @@ function initMap() {
                 stylers: [{ color: "#17263c" }],
             },
         ],
-    });
-    new google.maps.Marker({
-        position: { lat: 53.430, lng: -2.961 },
-        map,
-    });
+    }
 
+    // New map
+    const map = new google.maps.Map(document.getElementById("map"), options);
+
+
+    addMarker({ coords: { lat: 51.554, lng: -0.108 } });
+    addMarker({ coords: { lat: 52.509, lng: -1.884 } });
+    addMarker({ coords: { lat: 51.490, lng: -0.289 } });
+    addMarker({ coords: { lat: 50.861, lng: -0.083 } });
+    addMarker({ coords: { lat: 53.788, lng: -2.230 } });
+    addMarker({ coords: { lat: 51.481, lng: -0.191 } });
+    addMarker({ coords: { lat: 51.398, lng: -0.086 } });
+    addMarker({ coords: { lat: 53.438, lng: -2.966 } });
+    addMarker({ coords: { lat: 53.777, lng: -1.572 } });
+    addMarker({ coords: { lat: 52.620, lng: -1.142 } });
+    addMarker({ coords: { lat: 53.430, lng: -2.960 } });
+    addMarker({ coords: { lat: 53.483, lng: -2.200 } });
+    addMarker({ coords: { lat: 53.463, lng: -2.291 } });
+    addMarker({ coords: { lat: 54.975, lng: -1.621 } });
+    addMarker({ coords: { lat: 52.622, lng: -1.309 } });
+    addMarker({ coords: { lat: 50.905, lng: -1.391 } });
+    addMarker({ coords: { lat: 51.604, lng: -0.066 } });
+    addMarker({ coords: { lat: 51.649, lng: -0.401 } });
+    addMarker({ coords: { lat: 51.538, lng: -0.016 } });
+    addMarker({ coords: { lat: 52.590, lng: -2.130 } });
+
+
+
+    // Add marker function
+    function addMarker(props) {
+        const marker = new google.maps.Marker({
+            position: props.coords,
+            map: map,
+        });
+
+
+    };
+
+
+
+    /*
+        // New marker
+        const marker = new google.maps.Marker({
+            position: { lat: 53.430, lng: -2.961 },
+            map,
+        });
+    
+        const infoWindow = new google.maps.InfoWindow({
+            content: '<h4>Anfield Stadium</h4>'
+        });
+    
+        marker.addListener('click', function () {
+            infoWindow.open(map, marker);
+        });
+        */
 }
