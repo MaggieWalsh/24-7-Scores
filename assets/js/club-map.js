2ec9099b88aa731b796c34597eee10945bc47c14
Maggie Walsh <miggieholland@gmail.com>
2022-01-07 15:12:16 +0000

    Add infowindows to markers

--- assets/js/club-map.js
+++ assets/js/club-map.js
@@ -88,30 +88,107 @@ function initMap() {
         ],
     }
 
+    /*
+         // New marker
+         const marker = new google.maps.Marker({
+             position: { lat: 53.430, lng: -2.961 },
+             map,
+         });
+     
+         const infoWindow = new google.maps.InfoWindow({
+             content: '<h4>Anfield Stadium</h4>'
+         });
+     
+         marker.addListener('click', function () {
+             infoWindow.open(map, marker);
+         });
+         */
+
+
     // New map
     const map = new google.maps.Map(document.getElementById("map"), options);
 
 
-    addMarker({ coords: { lat: 51.554, lng: -0.108 } });
-    addMarker({ coords: { lat: 52.509, lng: -1.884 } });
-    addMarker({ coords: { lat: 51.490, lng: -0.289 } });
-    addMarker({ coords: { lat: 50.861, lng: -0.083 } });
-    addMarker({ coords: { lat: 53.788, lng: -2.230 } });
-    addMarker({ coords: { lat: 51.481, lng: -0.191 } });
-    addMarker({ coords: { lat: 51.398, lng: -0.086 } });
-    addMarker({ coords: { lat: 53.438, lng: -2.966 } });
-    addMarker({ coords: { lat: 53.777, lng: -1.572 } });
-    addMarker({ coords: { lat: 52.620, lng: -1.142 } });
-    addMarker({ coords: { lat: 53.430, lng: -2.960 } });
-    addMarker({ coords: { lat: 53.483, lng: -2.200 } });
-    addMarker({ coords: { lat: 53.463, lng: -2.291 } });
-    addMarker({ coords: { lat: 54.975, lng: -1.621 } });
-    addMarker({ coords: { lat: 52.622, lng: 1.309 } });
-    addMarker({ coords: { lat: 50.905, lng: -1.391 } });
-    addMarker({ coords: { lat: 51.604, lng: -0.066 } });
-    addMarker({ coords: { lat: 51.649, lng: -0.401 } });
-    addMarker({ coords: { lat: 51.538, lng: -0.016 } });
-    addMarker({ coords: { lat: 52.590, lng: -2.130 } });
+    addMarker({
+        coords: { lat: 51.554, lng: -0.108 },
+        content: '<h1>Arsenal</h1>'
+    });
+    addMarker({
+        coords: { lat: 52.509, lng: -1.884 },
+        content: '<h1>Aston Villa</h1>'
+    });
+    addMarker({
+        coords: { lat: 51.490, lng: -0.289 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 50.861, lng: -0.083 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 53.788, lng: -2.230 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 51.481, lng: -0.191 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 51.398, lng: -0.086 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 53.438, lng: -2.966 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 53.777, lng: -1.572 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 52.620, lng: -1.142 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 53.430, lng: -2.960 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 53.483, lng: -2.200 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 53.463, lng: -2.291 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 54.975, lng: -1.621 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 52.622, lng: 1.309 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 50.905, lng: -1.391 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 51.604, lng: -0.066 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 51.649, lng: -0.401 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 51.538, lng: -0.016 },
+        content: '<h1>Brentford</h1>'
+    });
+    addMarker({
+        coords: { lat: 52.590, lng: -2.130 },
+        content: '<h1>Brentford</h1>'
+    });
 
 
 
@@ -122,24 +199,16 @@ function initMap() {
             map: map,
         });
 
+        if (props.content) {
+            const infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
 
-    };
+            marker.addListener('click', function () {
+                infoWindow.open(map, marker);
+            });
+        }
 
+    }
 
-
-    /*
-        // New marker
-        const marker = new google.maps.Marker({
-            position: { lat: 53.430, lng: -2.961 },
-            map,
-        });
-    
-        const infoWindow = new google.maps.InfoWindow({
-            content: '<h4>Anfield Stadium</h4>'
-        });
-    
-        marker.addListener('click', function () {
-            infoWindow.open(map, marker);
-        });
-        */
 }
